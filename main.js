bb05c41d26ef8c31c6e0f514b8cafcfdc834d205
marco <marcosigurta.ovb@gmail.com>
2020-08-24 10:44:23 +0200

    Boolfix in progress

--- main.js
+++ main.js
@@ -23,17 +23,48 @@ function getMovie() {
             var target = $('#results');
             target.text('');
             var template = $('#template').html();
-            var compiled = Handlebars.compile(template); 
+            var compiled = Handlebars.compile(template);
 
             for (var i = 0; i < movies.length; i++) {
                 var movie = movies[i];
+                var vote = movie['vote_average'];
+
+                vote = Math.ceil(vote / 2);
+                var voteHTML = '';
+                
+                var poster = movie['poster_path'];
+                movie.poster = getPosterHTML(poster);
+
+                for (j = 0; j < 5; j++) {
+                    if (j < vote) {
+                        voteHTML += '<i class="fas fa-star" />'
+                    } else {
+                        voteHTML += '<i class="far fa-star" />'
+                    }
+                }
+
+                movie.stars = voteHTML;
+
+                var lang = movie['original_language'];
+                if (lang === 'it' || lang === 'en') {
+                    movie.flag = `<img class="flag" src="img/${lang}.png">`;
+
+                } else {
+                    movie.flag = lang;
+                }
+
+
                 var movieHTML = compiled(movie);
                 target.append(movieHTML)
             }
         }
-        
-   })
+
+    })
 }
+
+function getPosterHTML(poster) {
+
+};
 
 
 
